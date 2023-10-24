194658a9f119a81afff0247ccfb556898709f3b9
Vidya Srinivasan <subiksha.srinivasan.vidya@gmail.com>
2023-10-24 18:35:07 -0400

    hio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDoubleVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDoubleVision.java
@@ -4,6 +4,8 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Size;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -14,7 +16,6 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
-
 import java.util.List;
 
 /*
@@ -26,6 +27,7 @@ import java.util.List;
  */
 
 @TeleOp(name = "Double Vision")
+
 public class TestDoubleVision extends LinearOpMode {
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
 
@@ -93,6 +95,34 @@ public class TestDoubleVision extends LinearOpMode {
             sleep(20);
 
         }   // end while loop
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+// Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+// Choose a camera resolution. Not all cameras support all resolutions.
+        builder.setCameraResolution(new Size(640, 480));
+
+// Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        builder.enableLiveView(true);
+
+// Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+// Choose whether or not LiveView stops if no processors are enabled.
+// If set "true", monitor shows solid orange screen if no processors enabled.
+// If set "false", monitor shows camera view without annotations.
+        builder.setAutoStopLiveView(false);
+
+// Set and enable the processor.
+        builder.addProcessor(tfod);
+
+// Build the Vision Portal, using the above settings.
+        VisionPortal visionPortal = builder.build();
 
     }   // end method runOpMode()
 
